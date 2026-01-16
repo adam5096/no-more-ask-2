80e4c97a20424ec88d42df68ba33d4d0971445e7
adam5096 <adamchou5096@gmail.com>
2026-01-16 21:36:02 +0800

    chore: 新增 nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
   css: ['~/assets/style/main.css'],
-   postcss: {
+  postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
